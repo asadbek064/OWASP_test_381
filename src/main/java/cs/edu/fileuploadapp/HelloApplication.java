7fe28e20a275ce5f5b64ec0bfeeda9ae85d11dc4
Asadbek Karimov <asadbekasadbek@gmail.com>
2023-10-31 04:01:11 -0700

    Initial

---
+++ src/main/java/cs/edu/fileuploadapp/HelloApplication.java
@@ -0,0 +1,9 @@
+package cs.edu.fileuploadapp;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
